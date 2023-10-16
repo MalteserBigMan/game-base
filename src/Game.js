1fcca3c9a81981d6f763aa545c676e38e0081cf1
malte <malte.lindkvist@elev.ga.ntig.se>
2023-10-16 12:51:48 +0200

    Enemy, Slime och Game(uppdatering)

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,7 @@
 import UserInterface from './UserInterface.js'
 import Player from './Player.js'
 import InputHandler from './InputHandler.js'
+import Slime from './Slime.js'
 export default class Game {
   constructor(width, height) {
     this.width = width
@@ -12,19 +13,33 @@ export default class Game {
     this.gravity = 1
     this.debug = false
     this.player = new Player(this)
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 1000
   }
-  
+
 
   update(deltaTime) {
     this.player.update(deltaTime)
     if (!this.gameOver) {
       this.gameTime += deltaTime
+      if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+        this.addEnemy()
+        this.enemyTimer = 0
+      } else {
+        this.enemyTimer += deltaTime
+      }
+      this.enemies.forEach((enemy) => enemy.update(deltaTime))
+      this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
     }
   }
-
+  addEnemy() {
+    this.enemies.push(new Slime(this))
+  }
   draw(context) {
-        this.player.draw(context)
-      }
-    }
+    this.player.draw(context)
+    this.enemies.forEach((enemy) => enemy.draw(context))
+  }
+}
 
 
